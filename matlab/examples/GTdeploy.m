77b3552a3a48b8d5b2d742ae26d018990db77a06
Khalis Fadil <khalis_muse@yahoo.com>
2024-11-28 22:35:48 +0100

    Resolve merge conflicts

--- matlab/examples/GTdeploy.m
+++ matlab/examples/GTdeploy.m
@@ -3,14 +3,23 @@ username = 'target7';
 % address = '139.30.200.112';
 address = '139.30.200.83';
 %address = '10.0.10.32';
+<<<<<<< HEAD
 model = 'SimulinkModel';
+=======
+model = 'GenericTargetTemplate';
+>>>>>>> 4c66c70 (Resolve merge conflicts)
 
 target = GT.GenericTarget(username, address);
 
 target.terminateAtTaskOverload = false;
 target.terminateAtCPUOverload = false;
+<<<<<<< HEAD
 target.targetSoftwareDirectory = '~/home/target7/Khalis_ws/dynamicMapping/'; % bitte neue File
 target.targetBitmaskCPUCores = '0x1FFFFF';
+=======
+target.targetSoftwareDirectory = '~/Khalis_ws/dynamicMapping/'; % bitte neue File
+%target.targetBitmaskCPUCores = '0x1FFFFF';
+>>>>>>> 4c66c70 (Resolve merge conflicts)
 %target.additionalCompilerFlags.DEBUG_MODE = true;
 %target.portAppSocket = 65535;
 % target.DownloadAllData; % to download all log data
@@ -18,4 +27,8 @@ target.targetBitmaskCPUCores = '0x1FFFFF';
 %#########################################################
 %last deploy
 %target.GenerateCode;
+<<<<<<< HEAD
 target.Deploy(model);
+=======
+target.Deploy(model);
+>>>>>>> 4c66c70 (Resolve merge conflicts)
